9f2ef2d9cb2f81a221af00ed4fccac7a1021a64b
Michael N Mwanza <mwanza.n.m@gmail.com>
2022-04-10 14:50:44 +0200

    the last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: o
+ */
+
+int main(void)
+{
+
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n>5)
+	printf("Last digit of %zu 
+	return (0);
+}
