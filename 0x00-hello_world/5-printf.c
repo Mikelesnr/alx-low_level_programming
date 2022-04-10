c39992ba2110cc2438b7b199e685db7754fcdc6b
Michael N Mwanza <mwanza.n.m@gmail.com>
2022-04-10 14:17:20 +0200

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
- *Return: Always 0
+ * main - Entry point
+ * Return: 0
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
