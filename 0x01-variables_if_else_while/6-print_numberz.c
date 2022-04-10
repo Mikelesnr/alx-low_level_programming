ee1976a16271581b3d4c8057945681eb19c6c00d
Michael N Mwanza <mwanza.n.m@gmail.com>
2022-04-10 17:52:02 +0200

    print single digit base ten num with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - Print single digit based ten nums with putchar
+ *return: 0
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num);
+		num++;
+	}
+	
+	putchar("\n");
+
+	return (0);
+}
