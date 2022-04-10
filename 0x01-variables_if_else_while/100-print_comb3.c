ae2cc6a59133168b4d7d908edef6efd6ab0dfaae
Michael N Mwanza <mwanza.n.m@gmail.com>
2022-04-10 19:15:45 +0200

    print 2 digit combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,34 @@
-.
+#include <stdio.h>
+
+/**
+ *main - print 0-9 separated with commas, using putchar
+ *Return: 0
+ */
+
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '99')
+	{
+		if (n <= '9')
+		{
+			putchar('0' + n);
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			putchar(n);
+			if (n != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
