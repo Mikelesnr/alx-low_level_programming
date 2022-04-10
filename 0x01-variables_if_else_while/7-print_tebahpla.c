4d9b5ae1afd9d3c2f78b5da6504a9158add8e6ab
Michael N Mwanza <mwanza.n.m@gmail.com>
2022-04-10 18:28:49 +0200

    print alpha in reverse with putchar

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - print alphabet in reverse with putchar
+ *Return: 0
+ */
+
+int main(void)
+{
+	char alpha = 'z';
+
+	while (alpha >= 'a')
+	{
+		putchar(aplha);
+		char--;
+	}
+	putchar('\n);
+
+	return (0);
+}
