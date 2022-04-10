0008eb9e711778b6d110ad35a67a19134d8b21c0
Michael N Mwanza <mwanza.n.m@gmail.com>
2022-04-10 13:48:31 +0200

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- * returns: 0 always
+ *main - entry point
+ *returns: 0 always
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
