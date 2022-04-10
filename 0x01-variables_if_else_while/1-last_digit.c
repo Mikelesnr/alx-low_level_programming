651464d6f247fcfd86b71d398cf12eb933007d71
Michael N Mwanza <mwanza.n.m@gmail.com>
2022-04-10 15:50:06 +0200

    the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("Last digit of %i is %i and is ", n, (n % 10));
-	
+
 	if (n == 0)
 	printf("0\n");
 	else if (n < 6 && n != 0)
