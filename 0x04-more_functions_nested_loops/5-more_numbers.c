05ed2520118721ec92356fb6b523f7b32f83e3b4
Michael N Mwanza <mwanza.n.m@gmail.com>
2022-04-14 22:21:47 +0200

    more_numbers - prints 0 to 14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,11 @@ void more_numbers(void)
 
 	for (counter = 1; counter <= 10; counter++)
 	{
-		for (i = '0'; i <= '14'; i++)
+		for (i = 0; i <= 14; i++)
 		{
-			_putchar(i);
+			if (i / 10 > 0)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
 	}
