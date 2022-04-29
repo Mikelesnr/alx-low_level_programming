33f9253b4757623387a7c0e9cd54a3b71da4c372
Michael N Mwanza <mwanza.n.m@gmail.com>
2022-04-29 03:34:40 +0200

    factorial - find factorial of n

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,9 +6,11 @@
  * Return: factorial of number, -1 if negative n
  */
 
-unsigned int factorial(unsigned int n)
+int factorial(int n)
 {
-	if (n == 0 || n == 1)
+	if (n < 0)
+		return (-1);
+	else if (n == 0 || n == 1)
 		return (n);
 	else
 		return (n * factorial(n - 1));
