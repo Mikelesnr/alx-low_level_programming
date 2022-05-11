3737df5c3a682d38eaa1b3cdf33e2a2ab9a36809
Michael N Mwanza <mwanza.n.m@gmail.com>
2022-05-11 19:17:39 +0200

    print_name - execute a function given as a parameter

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
  * print_name - Function to call a function
  *
  * @name: Parameter 1
  * @f: parameter function
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (f != NULL)
