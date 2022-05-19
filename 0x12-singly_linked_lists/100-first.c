d449f920ae4d68e89b3a851297e2bc4e25c01db3
Michael N Mwanza <mwanza.n.m@gmail.com>
2022-05-19 21:30:46 +0200

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,23 +1,18 @@
-#include <stdio.h>
-
-/**
- * execute_before_main - executes this function before main function
- * Note: made possible by prior declaration of "__attribute__ ((constructor))"
+#include "lists.h"
+  
+/*
+ * File: 100-first.c
+ * Auth: Brennan D Baraban
  */
 
-void __attribute__ ((constructor)) execute_before_main()
-{
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
-}#include <stdio.h>
+void __attribute__((constructor)) hare(void);
 
 /**
- * execute_before_main - executes this function before main function
- * Note: made possible by prior declaration of "__attribute__ ((constructor))"
+ * hare - Prints a string before the
+ *        main function is executed.
  */
-
-void __attribute__ ((constructor)) execute_before_main()
+void hare(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
