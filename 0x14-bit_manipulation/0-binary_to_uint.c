b53ebfd01131a9a9e27c74e8b7d6ba38ff5a3e29
Michael N Mwanza <mwanza.n.m@gmail.com>
2022-05-26 20:58:21 +0200

    binary_to_uint - converts binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,36 +1,69 @@
-#include "main.h"
 
+#include "main.h"
 /**
- * binary_to_uint - converts binary to unsigned int
- * @b: binary
- * Return: unsigned int
+ * _pow - function that return the
+ * value of x raised to the power of y
+ *
+ * @x: parameter to base
+ * @y: parameter of pow
+ *
+ * Return: End program
+ */
+int _pow(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		y--;
+		x = x * _pow(x, y);
+		return (x);
+	}
+
+	return (0);
+}
+/**
+ * binary_to_uint - function that converts a binary number to an
+ * unsigned int
+ *
+ * @b: Pointer with the string whit the number to convert
+ *
+ * Return: The number converted
  */
 unsigned int binary_to_uint(const char *b)
 {
-
-	int len = 0, i;
 	unsigned int sum = 0;
+	int i, x, n;
 
 	if (b == NULL)
-		return (sum);
-
-	/* find string length */
-	while (b[len] != '\0')
-		len++;
-	len -= 1;
-
-	/* iterate string and if '1' then multiply by power of 2 */
-	/* get power of 2 via binary (e.g. 1<<2 = 100 in binary = 4) */
-	i = 0;
-	while (b[i])
 	{
-		if ((b[i] != '0') && (b[i] != '1'))
-			return (sum);
+		return (0);
+	}
 
-		if (b[i] == '1')
-			sum += (1 * (1 << len));
-		i++;
-		len--;
+	n = strlen(b) - 1;
+
+	/* Realizamos la operacion para hallar suma */
+	for (x = 0, i = 0; n >= x; n--)
+	{
+		switch (b[i])
+		{
+			case '1':
+				sum = sum + _pow(2, n);
+				i++;
+				break;
+			case '0':
+				sum = sum + 0;
+				i++;
+				break;
+			default:
+				return (0);
+		}
 	}
 
 	return (sum);
